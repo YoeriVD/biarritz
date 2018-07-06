2d8b9fd79cb93c414c01e4f5d98544e429b48b7c
Yoeri Van Damme <Yoeri.VanDamme@sdworx.com>
2018-07-06 17:31:00 +0200

    intro demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  person = 'everybody'
+  expenses = [
+    { description: 'Ticket to Biarritz', amount: 545.45, date: new Date(2018, 6, 28) },
+    { description: 'Taxi from Bilbao to Biarritz', amount: 250.05, date: new Date(2018, 7, 16) },
+    { description: 'Bottle of fien wine', amount: 45.90, date: new Date(2018, 7, 17) },
+  ]
+
+  deleteExpense(expense : any){
+    expense.deleted = true;
+  }
 }
