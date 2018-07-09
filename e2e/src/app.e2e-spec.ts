866e0306950707a79ee79b7351496664f9b788fb
Yoeri Van Damme <Yoeri.VanDamme@sdworx.com>
2018-07-09 12:40:17 +0200

    protractor

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,22 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display 3 items', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to biarritz!');
+    expect(page.expenses.count()).toEqual(3);
+  });
+  
+  it('should disable the submit button', () => {
+    page.navigateTo();
+    expect(page.submit.isEnabled()).toBeFalsy();
+  });
+  it('should be able to add an expense', () => {
+    page.navigateTo();
+    page.amount.sendKeys(15.55);
+    page.description.sendKeys('my desc');
+    page.date.sendKeys('01/01/2018')
+    page.submit.click();
+
+    expect(page.expenses.count()).toBe(4);
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -5,7 +5,10 @@ export class AppPage {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
-  }
+  get description() { return element(by.id('description')); }
+  get amount() { return element(by.id('amount')); }
+  get date() { return element(by.id('date')); }
+  get submit() { return element(by.css('button[type="submit"]')); }
+
+  get expenses() { return element.all(by.css('tbody tr')); }
 }
